f4d94213f13b04d18f6a5bb4f1ebe80fba65b804
ammriteshh <amritesh6767@gmail.com>
2024-09-26 23:32:31 +0530

    commit

--- Strings/IsomorphicStrings.cpp
+++ Strings/IsomorphicStrings.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char, char> m1, m2;
+        map<char, char> m1, m2; //two maps are created. these maps will store character mappings where m1 string's characters will mapped with t's characters and vice-versa
         if(s.size() != t.size()){
             return false;
         }
         for(int i=0; i<s.size();  i++){
-            if(m1.find(s[i]) != m1.end()){
-                if(m1[s[i]] != t[i]){
+            if(m1.find(s[i]) != m1.end()){ //if s[i] character exist in m1 map
+                if(m1[s[i]] != t[i]){ //if s[i] is mapped in m1 and its mapped value is not t[i]
                     return false;
                 }
             }
@@ -17,10 +17,11 @@ public:
                 }
             }
             else{
+                //if s[i] and t[i] are not mapped, we'll map it s[i] to t[i] and t[i] to s[i]
                 m1[s[i]] = t[i];
                 m2[t[i]] = s[i];
             }
         }
-        return true;
+        return true; //if after traversing all strings, there is no mismatch, then it is isomorphic{true}
     }
 };
