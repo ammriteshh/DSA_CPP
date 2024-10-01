1eb8cb097a3d8845cf837054d9639ef601fa1dbe
ammriteshh <amritesh6767@gmail.com>
2024-10-01 17:43:16 +0530

    commit

--- Arrays/LargestElement.cpp
+++ Arrays/LargestElement.cpp
@@ -1,10 +1,12 @@
-int getLargest(int arr[], int n)
-{
-    int res = 0;
-    for (int i = 0; i < n; i++)
-        if (arr[i] > arr[res])
-            res = i;
-            return res;
-}
-
-// Time Complexity = O(n)
+class Solution {
+public:
+    int largest(vector<int> &arr) {
+        int largest = arr[0];
+        for(int i=1; i<arr.size(); i++){
+            if(arr[i]>largest){
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
+};
