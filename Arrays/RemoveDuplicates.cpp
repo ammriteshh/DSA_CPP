117f0d5c45e15fbcf67171b20a4d8c9a09c07a58
ammriteshh <amritesh6767@gmail.com>
2024-10-03 17:45:59 +0530

    commit

--- Arrays/RemoveDuplicates.cpp
+++ Arrays/RemoveDuplicates.cpp
@@ -1,13 +1,18 @@
-int remDups(int arr[], int n)
-{
-    int res = 1;
-    for (int i = 1; i < n; i++)
-    {
-        if (arr[i] != arr[res - 1])
-        {
-            arr[res] = arr[i];
-            res++;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
         }
+
+        int k = 1; // Initialize the count of unique elements to 1
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) { //if current is not equal to previous, means it is unique
+                nums[k] = nums[i]; // Overwrite the next unique element
+                k++; // increment
+            }
+        }
+
+        return k;
     }
-    return res;
-}
+};
