dd2b43814aecfba4839af7f1179084018a9216ca
ammriteshh <amritesh6767@gmail.com>
2024-10-11 23:24:52 +0530

    commit

--- Arrays/BestBuy&Sell.cpp
+++ Arrays/BestBuy&Sell.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxProfit=0;
-        bestBuy = prices[0]; //assuming first day is best day to buy
+        int buyPrice = prices[0];
+        int profit = 0;
 
-        for(int i=0; i<prices.size(); i++){
-            if(price[i]>bestBuy){ //if current price is more than compared to bestBuy till now, we can calculate the profit
-                maxProfit = max(maxProfit, price[i] - bestBuy); //updating maxProfit, either existing maxProfit or the difference of current price and best buy
+        for (int i = 1; i < prices.size(); i++) {
+            if (buyPrice > prices[i]) { //Yeh condition check kar raha hai ki agar current day ka price (prices[i]) humari buying price se kam hai, toh hum buying price ko update karenge. Matlab hum naya minimum price dhundh rahe hain.
+                buyPrice = prices[i]; //Agar current price buyPrice se kam hai, toh hum isse buyPrice bana dete hain, taaki saste mein stock kharid sakein.
             }
-            bestBuy = min(bestBuy - price[i]); //updating bestBuy so that current price get low and we can buy
+
+            profit = max(profit, prices[i] - buyPrice); //Is line mein maximum profit calculate kar rahe hain. Hum naya profit calculate karte hain by subtracting buyPrice from prices[i] (matlab current selling price), aur max function use karke dekhte hain ki kya yeh naya profit purane profit se zyada hai.
         }
-        return maxProfit
+
+        return profit;
     }
 };
