2b77838a6dd97b005ba777995880310755ff16e2
ammriteshh <amritesh6767@gmail.com>
2024-10-19 23:10:08 +0530

    commit

--- Binary Search/NoOfOccurrence.cpp
+++ Binary Search/NoOfOccurrence.cpp
@@ -7,5 +7,5 @@ public:
 
 //Que: Given a sorted array Arr of size N and a number X, you need to find the number of occurrences of X in Arr.
 
-//What is LB doing? LB is searching for the element is greater than or equal to x
+//What is Lower Bound doing? LB is searching for the element is greater than or equal to x
 //What is UP doing? UP is searching for the the element is strictly greater than x
